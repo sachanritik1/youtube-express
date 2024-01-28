afc34593f8701000caf2e1001658b3b10b426fca
Ritik Sachan <sachanritik1@gmail.com>
2024-01-29 04:39:47 +0530

    Add cors middleware for handling cross-origin requests

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 import mainRouter from "./routes/mainRouter"
 
 const app = express()
 
+app.use(
+    cors({
+        origin: process.env.CLIENT_BASE_URL,
+        credentials: true,
+    })
+)
 app.use(express.json({ limit: "20kb" }))
 app.use(express.urlencoded({ extended: true, limit: "20kb" }))
 app.use(express.static("public"))
